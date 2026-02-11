9ca1c7b042a8dafbda7dd3a0082c8981b8c7e8ed
jhlee <jhlee89898989@gmail.com>
2026-02-11 15:46:56 +0900

    add: 통합 api 라우트 추가

---
+++ routes/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+
+const userRoutes = require("./users");
+const bookRoutes = require("./books");
+const cartRoutes = require("./carts");
+const likesRoutes = require("./likes");
+const orderRoutes = require("./orders");
+
+router.use("/users", userRoutes);
+router.use("/books", bookRoutes);
+router.use("/carts", cartRoutes);
+router.use("/likes", likesRoutes);
+router.use("/orders", orderRoutes);
+
+module.exports = router;

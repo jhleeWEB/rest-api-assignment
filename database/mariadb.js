e21851e67ce593ffa02d235bce809e2f7e92cfe6
jhlee <jhlee89898989@gmail.com>
2026-02-12 17:18:45 +0900

    add db 연결 코드 추가

---
+++ database/mariadb.js
@@ -0,0 +1,12 @@
+// Get the client
+const mysql = require("mysql2");
+
+// Create the connection to database
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "BookStore",
+});
+
+module.exports = connection;

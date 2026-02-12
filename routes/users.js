4b04fd7534ccae8b9775bd3aa478e7f3bac12a50
jhlee <jhlee89898989@gmail.com>
2026-02-12 17:19:49 +0900

    refact: 라우트 코드 리펙토링

--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,11 @@
 const express = require("express");
+const userController = require("../controllers/userController");
+
 const router = express.Router();
 router.use(express.json());
-router.post("/join", (req, res) => {
-  res.json("희원가입");
-});
-router.post("/login", (req, res) => {
-  res.json("로그인");
-});
-router.post("/reset", (req, res) => {
-  res.json("비번 재설정");
-});
-router.put("/reset", (req, res) => {
-  res.json("희원정보 수정");
-});
+router.post("/join", userController.join);
+router.post("/login", userController.login);
+router.post("/reset", userController.passwordResetRequest);
+router.put("/reset", userController.passwordReset);
 
 module.exports = router;
